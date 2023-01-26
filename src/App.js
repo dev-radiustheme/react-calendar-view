1f26014f870a2c62ba3226d9cc8d137518cca10e
ruhin-dev <99163828+ruhin-dev@users.noreply.github.com>
2023-01-26 09:51:08 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="main-wrapper">
+        <FullCalendar
+          eventClick={(e) => console.log(e.event)}
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          events={[
+            {
+              title: "Md. Ruhin Mia - Present",
+              date: "2023-01-18",
+              backgroundColor: "green",
+            },
+            {
+              title: "Present",
+              date: "2023-01-18",
+              backgroundColor: "green",
+            },
+            {
+              title: "Late",
+              date: "2023-01-18",
+              backgroundColor: "red",
+            },
+            {
+              title: "Late",
+              date: "2023-03-18",
+              backgroundColor: "red",
+            },
+          ]}
+        />
+      </div>
+    );
+  }
 }
-
-export default App;
